8886ca3ab3310c495fb252ac31bbee550732326d
Affan Hasan <affan.hasan@hotmail.com>
2012-08-28 12:55:46 +0500

    Update Test Case

--- src/main/java/org/jboss/as/quickstarts/kitchensink/controller/MemberRegistration.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/controller/MemberRegistration.java
@@ -1,5 +1,6 @@
 package org.jboss.as.quickstarts.kitchensink.controller;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
@@ -7,13 +8,15 @@ import javax.ejb.Stateful;
 import javax.enterprise.event.Event;
 import javax.enterprise.inject.Model;
 import javax.enterprise.inject.Produces;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 
+import org.jboss.as.quickstarts.kitchensink.model.Day;
+import org.jboss.as.quickstarts.kitchensink.model.Gunah;
 import org.jboss.as.quickstarts.kitchensink.model.Member;
+import org.jboss.as.quickstarts.kitchensink.model.Neky;
 
 // The @Stateful annotation eliminates the need for manual transaction demarcation
 @Stateful
@@ -47,11 +50,44 @@ public class MemberRegistration {
    public void register() throws Exception {
       log.info("Registering " + newMember.getName());
       em.persist(newMember);
-      facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful"));
+//    facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful"));
       memberEventSrc.fire(newMember);
+      
+      em.createQuery("select m from Member m order by m.name").getResultList();
       initNewMember();
+      
+//
+	   Day day = new Day();
+	   Date date = new Date();
+	   day.setDate(date);
+	   
+	   Neky neky = new Neky();
+	   neky.setDay(day);
+	   neky.setStartTime(date);
+	   neky.setDescription("Offered qaza Namaz");
+	   neky.setImprovementNote("Leave every thing at the time of namaz; but however if it is qaza then perform it at the first sigh of relief");
+	   
+	   Gunah gunah = new Gunah();
+	   gunah.setDay(day);
+	   gunah.setStartTime(date);
+	   gunah.setDescription("Became leaked");
+	   gunah.setRootCause("Lack of self control");
+	   gunah.setRemedy("Always control yourself and look before you leap");
+	   
+	   day.getGoodDeeds().add(neky);
+	   day.getBadDeeds().add(gunah);
+	   em.persist(day);
    }
-
+   
+	/**
+	 * @author AffanHasan
+	 * 
+	 * A method for testing entity persistence
+	 */
+   public void register(Day day) throws Exception {
+	   em.persist(day);
+   }
+	
    @PostConstruct
    public void initNewMember() {
       newMember = new Member();
--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
@@ -2,14 +2,23 @@ package org.jboss.as.quickstarts.kitchensink.test;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
+import javax.ejb.Stateful;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.quickstarts.kitchensink.controller.MemberRegistration;
+import org.jboss.as.quickstarts.kitchensink.model.BaseActivity;
+import org.jboss.as.quickstarts.kitchensink.model.BaseEntity;
+import org.jboss.as.quickstarts.kitchensink.model.Day;
+import org.jboss.as.quickstarts.kitchensink.model.Gunah;
+import org.jboss.as.quickstarts.kitchensink.model.Lagooh;
 import org.jboss.as.quickstarts.kitchensink.model.Member;
+import org.jboss.as.quickstarts.kitchensink.model.Neky;
 import org.jboss.as.quickstarts.kitchensink.util.Resources;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -23,7 +32,11 @@ public class MemberRegistrationTest {
    @Deployment
    public static Archive<?> createTestArchive() {
       return ShrinkWrap.create(WebArchive.class, "test.war")
-            .addClasses(Member.class, MemberRegistration.class, Resources.class)
+            .addClasses(
+            		Member.class, MemberRegistration.class, Resources.class
+            		, BaseEntity.class, Day.class, BaseActivity.class, Neky.class,
+            		Gunah.class, testSSB.class, Lagooh.class
+            		)
             .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
             .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
             // Deploy our test datasource
@@ -32,10 +45,10 @@ public class MemberRegistrationTest {
 
    @Inject
    MemberRegistration memberRegistration;
-
+   
    @Inject
    Logger log;
-
+   
    @Test
    public void testRegister() throws Exception {
       Member newMember = memberRegistration.getNewMember();
@@ -45,6 +58,40 @@ public class MemberRegistrationTest {
       memberRegistration.register();
       assertNotNull(newMember.getId());
       log.info(newMember.getName() + " was persisted with id " + newMember.getId());
+      
+//      testSSB testSSB = new testSSB();
+//      testSSB.persist();
+//      log.info("Persisted ");
    }
    
+   //Adding @Stateless annotation eliminates the need for manual transaction demarcation
+   @Stateful
+   public static class testSSB {
+	   
+	   @Inject
+	   private EntityManager em;
+	   
+	   public void persist(){
+		   Day day = new Day();
+		   Date date = new Date();
+		   day.setDate(date);
+		   
+		   Neky neky = new Neky();
+		   neky.setDay(day);
+		   neky.setStartTime(date);
+		   neky.setDescription("Offered qaza Namaz");
+		   neky.setImprovementNote("Leave every thing at the time of namaz; but however if it is qaza then perform it at the first sigh of relief");
+		   
+		   Gunah gunah = new Gunah();
+		   gunah.setDay(day);
+		   gunah.setStartTime(date);
+		   gunah.setDescription("Became leaked");
+		   gunah.setRootCause("Lack of self control");
+		   gunah.setRemedy("Always control yourself and look before you leap");
+		   
+		   day.getGoodDeeds().add(neky);
+		   day.getBadDeeds().add(gunah);
+		   em.persist(day);
+	   }
+   }
 }
